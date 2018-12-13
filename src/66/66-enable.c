68515abcd0186266cea2b4154b827e6743d6cb40
obarun <eric@obarun.org>
2018-12-13 12:31:32 +1100

    remove -d -I options

--- src/66/66-enable.c
+++ src/66/66-enable.c
@@ -119,7 +119,7 @@ int main(int argc, char const *const *argv,char const *const *envp)
 
 		for (;;)
 		{
-			int opt = getopt_args(argc,argv, ">hv:l:t:fd:I:S", &l) ;
+			int opt = getopt_args(argc,argv, ">hv:l:t:fS", &l) ;
 			if (opt == -1) break ;
 			if (opt == -2) strerr_dief1x(110,"options must be set first") ;
 			switch (opt)
