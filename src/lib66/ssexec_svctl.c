7349ebd2bf5405e563ce7e3d4c6e1a3cf1eb6334
obarun <eric@obarun.org>
2019-02-18 18:14:52 +1100

    use new ss_resolve_s struct

--- src/lib66/ssexec_svctl.c
+++ src/lib66/ssexec_svctl.c
@@ -40,13 +40,9 @@
 #include <66/utils.h>
 #include <66/tree.h>
 #include <66/ssexec.h>
+#include <66/resolve.h>
 
 //#include <stdio.h>
-
-
-static stralloc svkeep = STRALLOC_ZERO ;
-static genalloc gakeep = GENALLOC_ZERO ; //type svc_sig_s
-static stralloc saresolve = STRALLOC_ZERO ;
 
 unsigned int SV_DEADLINE = 3000 ;
 unsigned int DEATHSV = 10 ;
@@ -78,20 +74,17 @@ static int read_uint (char const *file, unsigned int *fd)
 
 /** @Return 0 on fail
  * @Return 1 on success */
-int handle_signal_svc(svc_sig *sv_signal)
+int handle_signal_svc(ss_resolve_sig_t *sv_signal)
 {
 	s6_svstatus_t status = S6_SVSTATUS_ZERO ;
-	char *sv = svkeep.s + sv_signal->scan ;
+	char *sv = sv_signal->res.sa.s + sv_signal->res.runat ;
 	if (!s6_svstatus_read(sv,&status))
 	{ 
 		VERBO3 strerr_warnwu2sys("read status of: ",sv) ;
 		return 0 ;
 	}
-	
-	int exitcode, exitstatus ;
-	exitcode = WIFEXITED(status.wstat) ;
-	exitstatus = WEXITSTATUS(status.wstat) ;
-	if (exitcode && exitstatus <= 1) return 1 ;
+	if (WIFSIGNALED(status.wstat) && !WEXITSTATUS(status.wstat) && (WTERMSIG(status.wstat) == 15 )) return 1 ;
+	if (!WIFSIGNALED(status.wstat) && !WEXITSTATUS(status.wstat)) return 1 ;
 	else return 0 ;
 }
 
@@ -107,7 +100,7 @@ static unsigned char const actions[9][9] =
     { WAIT,		GOTIT,	WAIT,	GOTIT,	WAIT,	WAIT,	PERM,	PERM,	UKNOW },// SIGRR
     { DEAD,		DEAD,	DEAD,	DEAD,	GOTIT,	DONE,	PERM,	PERM,	UKNOW },// SIGDOWN
     { DEAD,		DEAD,	DEAD,	DEAD,	WAIT,	GOTIT,	PERM,	PERM,	UKNOW },// SIGRDOWN
-    { DEAD,		DEAD,	DEAD,	DEAD,	WAIT,	WAIT,	GOTIT,	WAIT, 	UKNOW },// SIGX
+    { DEAD,		DEAD,	DEAD,	DEAD,	WAIT,	WAIT,	DONE,	WAIT, 	DONE },// SIGX
     { WAIT,		WAIT,	WAIT,	WAIT,	WAIT,	WAIT,	PERM,	GOTIT, 	UKNOW },// SIGO
     { UKNOW,	UKNOW,	UKNOW,	UKNOW,	UKNOW,	UKNOW,	UKNOW,	UKNOW, 	DONE },	// SIGSUP
     
@@ -165,7 +158,7 @@ static const uint8_t chtenum[128] =
  * @Return 2 on fail
  * @Return 3 for PERMANENT failure */
 
-int handle_case(char c, svc_sig *sv_signal)
+int handle_case(char c, ss_resolve_sig_t *sv_signal)
 {
 	int p, h, err ;
 	unsigned int state ;
@@ -210,9 +203,9 @@ int handle_case(char c, svc_sig *sv_signal)
 	return err ;
 }
 
-static int handle_signal_pipe(svc_sig *svc)
+static int handle_signal_pipe(ss_resolve_sig_t *svc)
 {
-	char *sv = svkeep.s + svc->scan ;
+	char *sv = svc->res.sa.s + svc->res.runat ;
 	for (;;)
 	{
 		switch (selfpipe_read())
@@ -230,18 +223,18 @@ static int handle_signal_pipe(svc_sig *svc)
 		}
 	}
 }
-static void announce(svc_sig *sv_signal)
+static void announce(ss_resolve_sig_t *sv_signal)
 {
 
 	int r = sv_signal->state ;
-	char *sv = svkeep.s + sv_signal->scan ;
+	char *sv = sv_signal->res.sa.s + sv_signal->res.runat ;
 	if (r == 3) { VERBO2 strerr_warnw3x(sv," report permanent failure to bring ",(sv_signal->sig > 1) ? "down" : "up") ; }
 	else if (r == 2) { VERBO2 strerr_warnwu3x("bring ", (sv_signal->sig > 3) ? "down " : "up ", sv) ; }
 	else if (r == 1) { VERBO2 strerr_warni4x(sv," is ",(sv_signal->sig > 3) ? "down" : "up"," but not notified by the daemon itself") ; }
 	else if (!r) { VERBO2 strerr_warni4x(sv,": ",(sv_signal->sig > 3) ? "stopped" : "started"," successfully") ; }
 }
 
-int svc_listen(genalloc *gasv, ftrigr_t *fifo,int spfd,svc_sig *svc)
+int svc_listen(genalloc *gasv, ftrigr_t *fifo,int spfd,ss_resolve_sig_t *svc)
 {
 	int r ;
 	iopause_fd x[2] = { { .fd = ftrigr_fd(fifo), .events = IOPAUSE_READ } , { .fd = spfd, .events = IOPAUSE_READ, .revents = 0 } } ;
@@ -250,7 +243,7 @@ int svc_listen(genalloc *gasv, ftrigr_t *fifo,int spfd,svc_sig *svc)
 	tain_now_g() ;
 	tain_add_g(&t,&svc->deadline) ;
 	
-	VERBO3 strerr_warnt2x("start iopause for: ",svkeep.s + svc->name) ;
+	VERBO3 strerr_warnt2x("start iopause for: ",svc->res.sa.s + svc->res.runat) ;
 	
 	for(;;)
 	{
@@ -266,7 +259,7 @@ int svc_listen(genalloc *gasv, ftrigr_t *fifo,int spfd,svc_sig *svc)
 		}
 		if (!(svc->ndeath))
 		{
-			VERBO2 strerr_warnw2x("number of try exceeded for: ",svkeep.s + svc->scan) ;
+			VERBO2 strerr_warnw2x("number of try exceeded for: ",svc->res.sa.s + svc->res.runat) ;
 			announce(svc) ;
 			break ;
 		}
@@ -274,7 +267,7 @@ int svc_listen(genalloc *gasv, ftrigr_t *fifo,int spfd,svc_sig *svc)
 		r = iopause_g(x, 2, &t) ;
 		if (r < 0){ VERBO3 strerr_warnwu1sys("iopause") ; return 0 ; }
 		/** timeout is not a error , the iopause process did right */
-		else if (!r) { VERBO3 strerr_warnt2x("timed out reached for: ",svkeep.s + svc->name) ; announce(svc) ; break ; } 
+		else if (!r) { VERBO3 strerr_warnt2x("timed out reached for: ",svc->res.sa.s + svc->res.runat) ; announce(svc) ; break ; } 
 		
 		if (x[1].revents & IOPAUSE_READ) return handle_signal_pipe(svc) ? 1 : 0 ;
 				
@@ -284,7 +277,7 @@ int svc_listen(genalloc *gasv, ftrigr_t *fifo,int spfd,svc_sig *svc)
 			svc->ndeath--;
 		}
 	}
-
+	
 	return 1 ;
 }
 
@@ -292,17 +285,15 @@ int ssexec_svctl(int argc, char const *const *argv,char const *const *envp,ssexe
 {
 	
 	// be sure that the global var are set correctly
-	svkeep = stralloc_zero ;
-	gakeep = genalloc_zero ; //type svc_sig_s
-	saresolve = stralloc_zero ;
 	SV_DEADLINE = 3000 ;
 	DEATHSV = 10 ;
+	tain_t ttmain ;
 	
-	int r, e, isup ;
+	int e, isup ;
 	unsigned int death, tsv ;
 	int SIGNAL = -1 ;
 	
-	svc_sig sv_signal = SVC_SIG_ZERO ;
+	genalloc gakeep = GENALLOC_ZERO ; //type ss_resolve_sig
 	
 	char *sig = 0 ;
 	
@@ -345,43 +336,20 @@ int ssexec_svctl(int argc, char const *const *argv,char const *const *envp,ssexe
 		
 	if ((scandir_ok(info->scandir.s)) !=1 ) strerr_dief3sys(111,"scandir: ", info->scandir.s," is not running") ;
 	
-
+	stralloc src = STRALLOC_ZERO ;
+	if (!ss_resolve_pointo(&src,info,SS_NOTYPE,SS_RESOLVE_SRC)) strerr_diefu1sys(111,"set revolve pointer to source") ;
 	for(;*argv;argv++)
 	{
-		char const *svname = *argv ;
-		size_t namelen = strlen(*argv) ;
-		size_t srclen ;
-		char src[info->tree.len + SS_SVDIRS_LEN + SS_SVC_LEN + 1 + namelen + 16 + 1] ;
-		memcpy(src,info->tree.s,info->tree.len) ;
-		memcpy(src + info->tree.len,SS_SVDIRS, SS_SVDIRS_LEN) ;
-		memcpy(src + info->tree.len + SS_SVDIRS_LEN, SS_SVC, SS_SVC_LEN) ;
-		src[info->tree.len + SS_SVDIRS_LEN + SS_SVC_LEN] = '/' ;
-		memcpy(src + info->tree.len + SS_SVDIRS_LEN + SS_SVC_LEN + 1,*argv,namelen) ;
-		src[info->tree.len + SS_SVDIRS_LEN + SS_SVC_LEN + 1 + namelen ] = 0 ;
-		srclen = info->tree.len + SS_SVDIRS_LEN + SS_SVC_LEN + 1 + namelen ;
-		
-		size_t svoklen ;
-		char svok[info->scandir.len + 1 + namelen + 1] ;
-		memcpy(svok,info->scandir.s,info->scandir.len) ;
-		svok[info->scandir.len] = '/' ;
-		memcpy(svok + info->scandir.len + 1 ,svname,namelen + 1) ;
-		svoklen = info->scandir.len + 1 + namelen ;
-					
-		/** do not check the logger*/
-		if (!resolve_pointo(&saresolve,info,0,SS_RESOLVE_SRC))
-		{
-			VERBO3 strerr_warnwu1x("set revolve pointer to source") ;
-			return 0 ;
-		}
-		if (resolve_read(&saresolve,saresolve.s,svname,"logger"))
-		{
-			if (str_diff(saresolve.s,svname))//0 mean equal
-			{
-				r = s6_svc_ok(svok) ;
-				if (r < 0) strerr_diefu2sys(111,"check ", svok) ;
-				if (!r)	strerr_dief2x(110,svok," is not initialized") ;
-			}
-		}
+		ss_resolve_sig_t sv_signal = RESOLVE_SIG_ZERO ;
+		char const *name = *argv ;
+		if (!ss_resolve_check(info,name,SS_RESOLVE_SRC)) strerr_dief2sys(110,"unknow service: ",name) ;
+		if (!ss_resolve_read(&sv_signal.res,src.s,name)) strerr_diefu2sys(111,"read resolve file of: ",name) ;
+		if (sv_signal.res.type >= BUNDLE) strerr_dief3x(111,name," has type ",get_keybyid(sv_signal.res.type)) ;
+		char *string = sv_signal.res.sa.s ;
+		char *svok = string + sv_signal.res.runat ;
+		size_t svoklen = strlen(svok) ;
+		char file[svoklen + 16 + 1] ;
+		memcpy(file,svok,svoklen) ;
 		
 		if (!s6_svstatus_read(svok,&status)) strerr_diefu2sys(111,"read status of: ",svok) ;
 		isup = status.pid && !status.flagfinishing ;
@@ -389,70 +357,62 @@ int ssexec_svctl(int argc, char const *const *argv,char const *const *envp,ssexe
 		if (isup && (SIGNAL <= SIGRUP))
 		{
 			VERBO2 strerr_warni2x(svok,": already up") ;
+			ss_resolve_free(&sv_signal.res) ;
 			continue ;
 		}
 		else if (!isup && (SIGNAL >= SIGDOWN))
 		{
 			VERBO2 strerr_warni2x(svok,": already down") ;
+			ss_resolve_free(&sv_signal.res) ;
 			continue ;
 		}
-		
-		sv_signal.scan = svkeep.len ;
-		if (!stralloc_catb(&svkeep, svok, svoklen + 1)) retstralloc(111,"main") ;
-		sv_signal.scanlen = svoklen ;
-				
-		sv_signal.name = svkeep.len ;
-		if (!stralloc_catb(&svkeep, svname, namelen + 1)) retstralloc(111,"main") ;
-		sv_signal.namelen = namelen ;
-		
-		sv_signal.src = svkeep.len ;
-		if (!stralloc_catb(&svkeep, src, srclen + 1)) retstralloc(111,"main") ;
-		sv_signal.srclen = srclen ;
 		
 		sv_signal.sigtosend = sig ;
 		sv_signal.sig = SIGNAL ;
 		
 		/** notification-fd */
-		memcpy(svok + svoklen,"/notification-fd",16) ;
-		svok[svoklen + 16] = 0 ;
+		
+		memcpy(file + svoklen,"/notification-fd",16) ;
+		file[svoklen + 16] = 0 ;
 		e = errno ;
 		errno = 0 ;
-		if (access(svok, F_OK) < 0)
+		
+		if (access(file, F_OK) < 0)
 		{
-			if (errno != ENOENT) strerr_diefu2sys(111, "access ", svok) ;
+			if (errno != ENOENT) strerr_diefu2sys(111, "access ", file) ;
 			if (SIGNAL == SIGRUP)
 			{
 				sv_signal.sig = SIGUP ;
 				sv_signal.sigtosend = "uwu" ;
-				VERBO2 strerr_warnw2x(svok, " is not present - ignoring request for readiness notification") ;
+				VERBO1 strerr_warnw2x(file, " is not present - ignoring request for readiness notification") ;
 			}
 			if (SIGNAL == SIGRR)
 			{
 				sv_signal.sig = SIGR ;
 				sv_signal.sigtosend = "uwr" ;
-				VERBO2 strerr_warnw2x(svok, " is not present - ignoring request for readiness notification") ;
+				VERBO1 strerr_warnw2x(file, " is not present - ignoring request for readiness notification") ;
 			}
 			if (SIGNAL == SIGRDOWN)
 			{
 				sv_signal.sig = SIGDOWN ;
 				sv_signal.sigtosend = "dwd" ;
-				VERBO2 strerr_warnw2x(svok, " is not present - ignoring request for readiness notification") ;
+				VERBO1 strerr_warnw2x(file, " is not present - ignoring request for readiness notification") ;
 			}		
 		}
 		else
 		{
-			if (!read_uint(svok,&sv_signal.notify)) strerr_diefu2sys(111,"read: ",svok) ;
+			if (!read_uint(file,&sv_signal.notify)) strerr_diefu2sys(111,"read: ",file) ;
 		}
 		
 		/** max-death-tally */
 		if (!death)
 		{
-			memcpy(svok + svoklen,"/max-death-tally", 16) ;
-			svok[svoklen + 16] = 0 ;
+			memcpy(file + svoklen,"/max-death-tally", 16) ;
+			file[svoklen + 16] = 0 ;
 			errno = 0 ;
-			if (access(svok, F_OK) < 0)
+			if (access(file, F_OK) < 0)
 			{
-				if (errno != ENOENT) strerr_diefu2sys(111, "access ", svok) ;
+				if (errno != ENOENT) strerr_diefu2sys(111, "access ", file) ;
 			}
 			if (errno == ENOENT)
 			{
@@ -460,7 +420,7 @@ int ssexec_svctl(int argc, char const *const *argv,char const *const *envp,ssexe
 			}
 			else
 			{
-				if (!read_uint(svok,&sv_signal.ndeath)) strerr_diefu2sys(111,"read: ",svok) ;
+				if (!read_uint(file,&sv_signal.ndeath)) strerr_diefu2sys(111,"read: ",file) ;
 			}
 		}
 		else sv_signal.ndeath = death ;
@@ -469,7 +429,6 @@ int ssexec_svctl(int argc, char const *const *argv,char const *const *envp,ssexe
 		tain_from_millisecs(&tcheck,tsv) ;
 		int check = tain_to_millisecs(&tcheck) ;
 		if (check > 0) tain_from_millisecs(&sv_signal.deadline, tsv) ;
-			
 		else
 		{
 			/** timeout-{up/down} */
@@ -485,10 +444,12 @@ int ssexec_svctl(int argc, char const *const *argv,char const *const *envp,ssexe
 					tm="/timeout-down" ;
 				}
 				errno = 0 ;
-				memcpy(svok + svoklen,tm, strlen(tm) + 1) ;
-				if (access(svok, F_OK) < 0)
+				size_t tmlen = strlen(tm) ;
+				memcpy(file + svoklen,tm, tmlen) ;
+				file[svoklen + tmlen] = 0 ;
+				if (access(file, F_OK) < 0)
 				{
-					if (errno != ENOENT) strerr_diefu2sys(111, "access ", svok) ;
+					if (errno != ENOENT) strerr_diefu2sys(111, "access ", file) ;
 				}
 				if (errno == ENOENT)
 				{
@@ -496,7 +457,7 @@ int ssexec_svctl(int argc, char const *const *argv,char const *const *envp,ssexe
 				}
 				else
 				{
-					if (!read_uint(svok,&t)) strerr_diefu2sys(111,"read: ",svok) ;
+					if (!read_uint(file,&t)) strerr_diefu2sys(111,"read: ",file) ;
 					tain_from_millisecs(&sv_signal.deadline, t) ;
 				}
 			}
@@ -504,12 +465,12 @@ int ssexec_svctl(int argc, char const *const *argv,char const *const *envp,ssexe
 		
 		errno = e ;
 		
-		if (!genalloc_append(svc_sig,&gakeep,&sv_signal)) strerr_diefu1sys(111,"append genalloc") ;
-				
+		if (!genalloc_append(ss_resolve_sig_t,&gakeep,&sv_signal)) strerr_diefu1sys(111,"append genalloc") ;
+		
 	}
 	
 	/** nothing to do */
-	if (!genalloc_len(svc_sig,&gakeep))	goto finish ;
+	if (!genalloc_len(ss_resolve_sig_t,&gakeep)) goto finish ;
 	
 	int spfd = selfpipe_init() ;
 	if (spfd < 0) strerr_diefu1sys(111, "selfpipe_trap") ;
@@ -517,20 +478,36 @@ int ssexec_svctl(int argc, char const *const *argv,char const *const *envp,ssexe
 	if (selfpipe_trap(SIGTERM) < 0) strerr_diefu1sys(111, "selfpipe_trap") ;
 	if (sig_ignore(SIGPIPE) < 0) strerr_diefu1sys(111,"ignore SIGPIPE") ;
 		
-	if (!svc_init_pipe(&fifo,&gakeep,&svkeep)) strerr_diefu1x(111,"init pipe") ;
+	if (!svc_init_pipe(&fifo,&gakeep)) strerr_diefu1x(111,"init pipe") ;
 
-	for (unsigned int i = 0 ; i < genalloc_len(svc_sig,&gakeep) ; i++)
+	for (unsigned int i = 0 ; i < genalloc_len(ss_resolve_sig_t,&gakeep) ; i++)
 	{ 
-		svc_sig *sv = &genalloc_s(svc_sig,&gakeep)[i] ;
+		ss_resolve_sig_t *sv = &genalloc_s(ss_resolve_sig_t,&gakeep)[i] ;
 		if (!svc_listen(&gakeep,&fifo,spfd,sv)) strerr_diefu1x(111,"listen pipe") ;
+		ss_resolve_free(&sv->res) ;
 	}
 	
-	finish:
-		ftrigr_end(&fifo) ;
-		selfpipe_finish() ;
-		stralloc_free(&svkeep) ;
-		genalloc_free(svc_sig,&gakeep) ;
+	tain_now_g() ;
+	tain_addsec(&ttmain,&STAMP,2) ;
 	
+	for (unsigned int i = 0 ; i < genalloc_len(ss_resolve_sig_t,&gakeep) ; i++)
+	{ 
+		ss_resolve_sig_t *sv = &genalloc_s(ss_resolve_sig_t,&gakeep)[i] ;
+		if (!ftrigr_unsubscribe_g(&fifo, sv->ids, &ttmain))
+		{ 
+			VERBO3 strerr_warnwu1sys("unsubscribe to fifo of") ;
+			return 0 ;
+		}
+	}
+	
+	ftrigr_end(&fifo) ;
+	selfpipe_finish() ;
+	
+	finish:
+		stralloc_free(&src) ;
+		genalloc_deepfree(ss_resolve_sig_t,&gakeep,ss_resolve_free) ;
+		//ss_resolve_free(&sv_signal.res) ;
+		
 	return 0 ;		
 }
 
