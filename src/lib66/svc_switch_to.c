c360c85fdfe6c5840d39df548f60875c4125b7c5
obarun <eric@obarun.org>
2019-02-24 03:36:05 +1100

    fix verbosity output

--- src/lib66/svc_switch_to.c
+++ src/lib66/svc_switch_to.c
@@ -29,8 +29,8 @@ int svc_switch_to(ssexec_t *info,unsigned int where)
 	int r ;
 	
 	char type[UINT_FMT] ;
-	type[uint_fmt(type, CLASSIC)] = 0 ;
-	size_t typelen = strlen(type) ;
+	size_t typelen = uint_fmt(type, CLASSIC) ;
+	type[typelen] = 0 ;
 	size_t cmdlen ;
 	char cmd[typelen + 6 + 1] ;
 	memcpy(cmd,"-t",2) ;
@@ -42,41 +42,41 @@ int svc_switch_to(ssexec_t *info,unsigned int where)
 	r = backup_cmd_switcher(VERBOSITY,cmd,info) ;
 	if (r < 0)
 	{
-		VERBO3 strerr_warnwu2sys("find origin of svc service for: ",info->treename) ;
+		VERBO3 strerr_warnwu2sys("find origin of svc service for: ",info->treename.s) ;
 		return 0 ;
 	}
 	// point to origin
 	if (!r && where)
 	{
-		VERBO3 strerr_warnt2x("make a backup of svc service for: ",info->treename) ;
+		VERBO3 strerr_warnt2x("make a backup of svc service for: ",info->treename.s) ;
 		if (!backup_make_new(info,CLASSIC))
 		{
-			VERBO3 strerr_warnwu2sys("make a backup of svc service for: ",info->treename) ;
+			VERBO3 strerr_warnwu2sys("make a backup of svc service for: ",info->treename.s) ;
 			return 0 ;
 		}
-		VERBO3 strerr_warnt3x("switch svc service for tree: ",info->treename," to backup") ;
+		VERBO3 strerr_warnt3x("switch svc service for tree: ",info->treename.s," to backup") ;
 		memcpy(cmd + cmdlen," -s1",4) ;
 		cmd[cmdlen + 4] = 0 ;
 		r = backup_cmd_switcher(VERBOSITY,cmd,info) ;
 		if (r < 0)
 		{
-			VERBO3 strerr_warnwu3sys("switch svc service for: ",info->treename," to backup") ;
+			VERBO3 strerr_warnwu3sys("switch svc service for: ",info->treename.s," to backup") ;
 		}
 	}
 	else if (r > 0 && !where)
 	{
-		VERBO3 strerr_warnt3x("switch svc service for tree: ",info->treename," to source") ;
+		VERBO3 strerr_warnt3x("switch svc service for tree: ",info->treename.s," to source") ;
 		memcpy(cmd + cmdlen," -s0",4) ;
 		cmd[cmdlen + 4] = 0 ;
 		r = backup_cmd_switcher(VERBOSITY,cmd,info) ;
 		if (r < 0)
 		{
-			VERBO3 strerr_warnwu3sys("switch svc service for: ",info->treename," to source") ;
+			VERBO3 strerr_warnwu3sys("switch svc service for: ",info->treename.s," to source") ;
 		}
-		VERBO3 strerr_warnt2x("make a backup of svc service for: ",info->treename) ;
+		VERBO3 strerr_warnt2x("make a backup of svc service for: ",info->treename.s) ;
 		if (!backup_make_new(info,CLASSIC))
 		{
-			VERBO3 strerr_warnwu2sys("make a backup of svc service for: ",info->treename) ;
+			VERBO3 strerr_warnwu2sys("make a backup of svc service for: ",info->treename.s) ;
 			return 0 ;
 		}
 	}
