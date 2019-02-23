15a674d9030e0c924e81029154ef3005c527a32a
obarun <eric@obarun.org>
2019-02-24 03:24:02 +1100

    reverse the dependencies for the master bundle

--- src/lib66/graph.c
+++ src/lib66/graph.c
@@ -314,7 +314,7 @@ int graph_master(genalloc *ga, graph_t *g)
 	}
 	
 	for (w = 0 ; w < g->nvertex ; w++)
-		if (c[w] == WHITE)
+		if (c[w] == BLACK)
 			if (!stra_add(ga,string + genalloc_s(vertex_graph_t,&g->stack)[w].name)) 
 				return 0 ;
 			 
