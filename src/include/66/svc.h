0dfe9b6acec94939275656813c3f990d44f082a6
obarun <eric@obarun.org>
2019-02-24 03:20:56 +1100

    add svc_send

--- src/include/66/svc.h
+++ src/include/66/svc.h
@@ -102,5 +102,5 @@ extern int svc_switch_to(ssexec_t *info,unsigned int where) ;
 extern int svc_init(ssexec_t *info,char const *src, genalloc *ga) ;
 extern int svc_init_pipe(ftrigr_t *fifo,genalloc *gasv) ;
 extern int svc_shutnremove(ssexec_t *info, genalloc *ga,char const *sig,  char const *const *envp) ;
-
+extern int svc_send(ssexec_t *info,genalloc *ga,char const *sig,char const *const *envp) ;
 #endif
