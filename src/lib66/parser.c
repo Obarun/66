060bfd5c74fa019b35192d6a3c9ecd1b2be0cf2a
obarun <eric@obarun.org>
2019-03-04 14:35:58 +1100

    announce after the add

--- src/lib66/parser.c
+++ src/lib66/parser.c
@@ -85,8 +85,6 @@ static int add_sv(sv_alltype *sv_before,char const *name,unsigned int *nbsv)
 {
 	int r ;
 	
-	VERBO1 strerr_warni2x("Service parsed successfully: ",name) ;
-	
 	if (!genalloc_append(sv_alltype,&gasv,sv_before)) retstralloc(0,"add_sv") ;
 	
 	r = add_cname(&ganame,&deps_map,name,sv_before) ;
@@ -96,6 +94,8 @@ static int add_sv(sv_alltype *sv_before,char const *name,unsigned int *nbsv)
 		return 0 ;
 	}
 	(*nbsv)++ ;
+	
+	VERBO1 strerr_warni2x("Service parsed successfully: ",name) ;
 	
 	return 1 ;
 }
