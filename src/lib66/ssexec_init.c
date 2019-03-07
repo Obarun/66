dfb6ed6f10b7e9c2b50b4b1a5436e5ac4566a282
obarun <eric@obarun.org>
2019-03-08 08:30:30 +1100

    create live/state/treename directory, respect timeout

--- src/lib66/ssexec_init.c
+++ src/lib66/ssexec_init.c
@@ -53,8 +53,10 @@ int ssexec_init(int argc, char const *const *argv,char const *const *envp,ssexec
 	pid_t pid ;
 	
 	genalloc gasvc = GENALLOC_ZERO ; //stralist type
-	
 	genalloc gares = GENALLOC_ZERO ; //ss_resolve_t type
+	stralloc ressrc = STRALLOC_ZERO ;
+	stralloc resdst = STRALLOC_ZERO ;
+	stralloc src = STRALLOC_ZERO ;
 	ss_resolve_t res = RESOLVE_ZERO ;
 	
 	classic = db = earlier = 0 ;
@@ -92,9 +94,33 @@ int ssexec_init(int argc, char const *const *argv,char const *const *envp,ssexec
 	svdirlen = info->tree.len + SS_SVDIRS_LEN ;
 	memcpy(svdir + svdirlen, SS_SVC ,SS_SVC_LEN) ;
 	svdir[svdirlen +  SS_SVC_LEN] = 0 ;
-
-	stralloc src = STRALLOC_ZERO ;
+	
+	/** resolve live dir*/
 	if (!ss_resolve_pointo(&src,info,SS_NOTYPE,SS_RESOLVE_SRC)) strerr_diefu1sys(111,"set revolve pointer to source") ;
+	
+	if (!stralloc_copy(&ressrc,&src)) retstralloc(111,"main") ;
+	ressrc.len--;
+	if (!stralloc_cats(&ressrc,SS_RESOLVE)) retstralloc(111,"main") ;
+	if (!stralloc_0(&ressrc)) retstralloc(111,"main") ;
+		
+	if (!ss_resolve_pointo(&src,info,SS_NOTYPE,SS_RESOLVE_LIVE)) strerr_diefu1sys(111,"set revolve pointer to source") ;
+	r = scan_mode(src.s,S_IFDIR) ;
+	if (r < 0) strerr_dief2x(111,src.s," conflicting format") ;
+	if (!r)
+	{
+		VERBO2 strerr_warni3x("create directory: ",src.s,SS_RESOLVE+1) ;
+		r = dir_create_under(src.s,SS_RESOLVE+1,0700) ;
+		if (!r) strerr_diefu3sys(111,"create directory: ",src.s,SS_RESOLVE+1) ;
+	}
+	if (!stralloc_copy(&resdst,&src)) retstralloc(111,"main") ;
+	resdst.len--;
+	if (!stralloc_cats(&resdst,SS_RESOLVE)) retstralloc(111,"main") ;
+	if (!stralloc_0(&resdst)) retstralloc(111,"main") ;
+	
+	if (!hiercopy(ressrc.s,resdst.s)) strerr_diefu4sys(111,"copy resolve file of: ",ressrc.s," to ",resdst.s) ;
+		
+	stralloc_free(&ressrc) ;
+	stralloc_free(&resdst) ;
 	/** svc already initiated */
 	if (classic)
 	{
@@ -106,18 +132,18 @@ int ssexec_init(int argc, char const *const *argv,char const *const *envp,ssexec
 			VERBO1 strerr_warni3x("svc service of tree: ",info->treename.s," already initiated") ;
 			goto follow ;
 		}
+				
 		for (i = 0 ; i < genalloc_len(stralist,&gasvc) ; i++)
 		{
 			char *name = gaistr(&gasvc,i) ;
 			ss_resolve_t tmp = RESOLVE_ZERO ;
-			if (!ss_resolve_check(info,name,SS_RESOLVE_SRC)) strerr_dief2sys(110,"unknow service: ",name) ;
+			if (!ss_resolve_check(info,name,SS_RESOLVE_LIVE)) strerr_dief2sys(110,"unknow service: ",name) ;
 			if (!ss_resolve_read(&tmp,src.s,name)) strerr_diefu2sys(111,"read resolve file of: ",name) ;
-			
 			if (!genalloc_append(ss_resolve_t,&gares,&tmp)) strerr_diefu3x(111,"add: ",name," on genalloc") ;
-			
+			if (!ss_resolve_add_deps(&gares,&tmp,info)) strerr_diefu2sys(111,"resolve dependencies of: ",name) ;	
 		}
-		if (!ss_resolve_addlogger(info,&gares))	strerr_diefu1sys(111,"add logger to list") ;
-		
+		/** reverse to start first the logger */
+		genalloc_reverse(ss_resolve_t,&gares) ;
 		if (!earlier)
 		{
 			if (!svc_init(info,svdir,&gares)) strerr_diefu2x(111,"initiate service of tree: ",info->treename.s) ;
@@ -178,7 +204,10 @@ int ssexec_init(int argc, char const *const *argv,char const *const *envp,ssexec
 		prefix[info->treename.len + 1] = 0 ;
 		
 		{
-			char const *newargv[10] ;
+			char tt[UINT32_FMT] ;
+			tt[uint32_fmt(tt,info->timeout)] = 0 ;
+			
+			char const *newargv[12] ;
 			unsigned int m = 0 ;
 			
 			newargv[m++] = S6RC_BINPREFIX "s6-rc-init" ;
@@ -188,22 +217,24 @@ int ssexec_init(int argc, char const *const *argv,char const *const *envp,ssexec
 			newargv[m++] = svdir ;
 			newargv[m++] = "-p" ;
 			newargv[m++] = prefix ;
+			newargv[m++] = "-t" ;
+			newargv[m++] = tt ;
 			newargv[m++] = "--" ;
 			newargv[m++] = info->scandir.s ;
 			newargv[m++] = 0 ;
 			
-			VERBO2 strerr_warni3x("initiate db ",svdir," ...") ;
+			VERBO2 strerr_warni3x("initiate db of tree: ",info->treename.s," ...") ;
 			
 			pid = child_spawn0(newargv[0],newargv,envp) ;
 			if (waitpid_nointr(pid,&wstat, 0) < 0)
 				strerr_diefu2sys(111,"wait for ",newargv[0]) ;
 				
 			if (wstat)
-				strerr_diefu2x(111,"init db: ",svdir) ;
+				strerr_diefu2x(111,"init db of tree: ",info->treename.s) ;
 		}
 	}
 	end:	
-	
 	genalloc_deepfree(stralist,&gasvc,stra_free) ;
+	
 	return 0 ;
 }
