c3c225f986a9f28e9da9f0af0510e2a82f5d0420
obarun <eric@obarun.org>
2019-04-01 17:34:00 +1100

    clean headers file

--- src/lib66/ssexec_enable.c
+++ src/lib66/ssexec_enable.c
@@ -13,37 +13,26 @@
  */
  
 #include <string.h>
-#include <sys/stat.h>
-#include <stdlib.h>
+#include <errno.h>
+//#include <stdio.h>
 
 #include <oblibs/obgetopt.h>
 #include <oblibs/error2.h>
-#include <oblibs/directory.h>
-#include <oblibs/files.h>
-#include <oblibs/string.h>
-#include <oblibs/types.h>
 #include <oblibs/stralist.h>
 
 #include <skalibs/stralloc.h>
 #include <skalibs/genalloc.h>
 #include <skalibs/djbunix.h>
-#include <skalibs/buffer.h>
-#include <skalibs/direntry.h>
-#include <skalibs/unix-transactional.h>
 #include <skalibs/diuint32.h>
 
 #include <66/constants.h>
 #include <66/utils.h>
-#include <66/enum.h>
 #include <66/tree.h>
 #include <66/db.h>
 #include <66/parser.h>
-#include <66/backup.h>
 #include <66/svc.h>
 #include <66/resolve.h>
 #include <66/ssexec.h>
-
-#include <stdio.h>
 
 static unsigned int FORCE = 0 ;
 
