be802813b9ef7f585dafcd35a10689f2e558cdd0
obarun <eric@obarun.org>
2019-06-18 12:34:14 +1100

    please compiler do not complain

---
+++ src/lib66/ss_utils.c
@@ -0,0 +1,17 @@
+/* 
+ * ss_utils.c
+ * 
+ * Copyright (c) 2018-2019 Eric Vidal <eric@obarun.org>
+ * 
+ * All rights reserved.
+ * 
+ * This file is part of Obarun. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this
+ * distribution.
+ * This file may not be copied, modified, propagated, or distributed
+ * except according to the terms contained in the LICENSE file./
+ * */
+ 
+#include <66/utils.h>
+
+unsigned int VERBOSITY = 1 ;
