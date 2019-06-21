99d44f1b4892f86714a9eff18eee5267be56df7c
obarun <eric@obarun.org>
2019-06-21 16:51:25 +1100

    pass to oblibs library

--- src/extra-tools/execl-envfile.c
+++ src/extra-tools/execl-envfile.c
@@ -15,10 +15,12 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #include <oblibs/string.h>
 #include <oblibs/stralist.h>
 #include <oblibs/error2.h>
+#include <oblibs/types.h>
 #include <oblibs/obgetopt.h>
 #include <oblibs/directory.h>
 #include <oblibs/files.h>
@@ -37,20 +39,19 @@
 #include <66/parser.h>
 #include <66/environ.h>
 
-unsigned int VERBOSITY = 1 ;
 static stralloc SAENV = STRALLOC_ZERO ;
 static genalloc GAENV = GENALLOC_ZERO ; //diuint32, pos in senv
 
-#define USAGE "execl-envfile [ -h help ] [ -f file ] [ -l ] dir prog"
+#define USAGE "execl-envfile [ -h help ] [ -l ] src prog"
 
 static inline void info_help (void)
 {
   static char const *help =
-"execl-envfile <options> dir prog\n"
+"execl-envfile <options> src prog\n"
 "\n"
 "options :\n"
 "	-h: print this help\n" 
-"	-f: file to parse\n"
+"	-f: file to parse(deprecated)\n"
 "	-l: loose\n"
 ;
 
@@ -58,14 +59,24 @@ static inline void info_help (void)
     strerr_diefu1sys(111, "write to stdout") ;
 }
 
+int loop_stra(stralloc *sa,char const *search)
+{
+	size_t i = 0, len = sa->len ;
+	for (;i < len; i += strlen(sa->s + i) + 1)
+		if (!strcmp(sa->s+i,search)) return 1 ;
+	
+	return 0 ;
+}
+
 int main (int argc, char const *const *argv, char const *const *envp)
 {
-	int r, i, one, unexport  ;
+	int r, i, unexport  ;
 	int insist = 1 ;
-	
+	size_t pathlen ;
 	char const *path = 0 ;
 	char const *file = 0 ;
-	
+	char tpath[MAXENV + 1] ;
+	char tfile[MAXENV+1] ;
 	stralloc src = STRALLOC_ZERO ;
 	stralloc modifs = STRALLOC_ZERO ;
 	stralloc dst = STRALLOC_ZERO ;
@@ -73,7 +84,7 @@ int main (int argc, char const *const *argv, char const *const *envp)
 	
 	exlsn_t info = EXLSN_ZERO ;
 	
-	r = i = one = unexport = 0 ;
+	r = i = unexport = 0 ;
 	insist = 1 ;
 	
 	PROG = "execl-envfile" ;
@@ -88,7 +99,7 @@ int main (int argc, char const *const *argv, char const *const *envp)
 			switch (opt)
 			{
 				case 'h' : 	info_help(); return 0 ;
-				case 'f' :  file = l.arg ; one = 1 ; break ;
+				case 'f' :  file = l.arg ;  break ;
 				case 'l' : 	insist = 0 ; break ;
 				default : exitusage(USAGE) ; 
 			}
@@ -101,7 +112,44 @@ int main (int argc, char const *const *argv, char const *const *envp)
 	path = *argv ;
 	argv++;
 	argc--;
-	if (path[0] != '/') strerr_dief3x(111,"directory: ",path," must be an absolute path") ;
+	
+	/** Mark -f as deprecated */	
+	if (file)
+	{
+		strerr_warnw1x("-f options is deprecated") ;
+		if (path[0] != '/') strerr_dief3x(111,"path of: ",path,": must be absolute") ;
+	}
+	else
+	{
+		r = scan_mode(path,S_IFREG) ;
+		if (!r) strerr_diefu2sys(111,"find: ",path) ;
+		if (r < 0)
+		{
+			r = scan_mode(path,S_IFDIR) ;
+			if (!r) strerr_diefu2sys(111,"find: ",path) ;
+			if (r < 0) strerr_dief2x(111,"invalid format of: ", path) ;
+			if (path[0] == '.')
+			{
+				if (!dir_beabsolute(tpath,path)) 
+					strerr_diefu2sys(111,"find absolute path of: ",path) ;
+			}
+			else if (path[0] != '/') strerr_dief3x(111,"path of: ",path,": must be absolute") ;
+			else
+			{
+				pathlen = strlen(path) ;
+				memcpy(tpath,path,pathlen);
+				tpath[pathlen] = 0 ;
+			}
+			path = tpath ;
+		}
+		else
+		{
+			if (!basename(tfile,path)) strerr_diefu2x(111,"get file name of: ",path) ;
+			file = tfile ;
+			if (!dirname(tpath,path)) strerr_diefu2x(111,"get parent path of: ",path) ;
+			path = tpath ;
+		}
+	}
 	
 	r = dir_get(&toparse,path,"",S_IFREG) ;
 	if (!r && insist) strerr_diefu2sys(111,"get file from: ",path) ;
@@ -110,7 +158,7 @@ int main (int argc, char const *const *argv, char const *const *envp)
 		xpathexec_run(argv[0],argv,envp) ;
 	}
 	
-	if (one)
+	if (file)
 	{
 		r = stra_findidx(&toparse,file) ;
 		if (r < 0) 
@@ -130,10 +178,12 @@ int main (int argc, char const *const *argv, char const *const *envp)
 		for (i = 0 ; i < genalloc_len(stralist,&toparse) ; i++)
 		{
 			src.len = 0 ;
+			size_t n = i + MAXVAR ;
 			if (i > MAXFILE) strerr_dief2x(111,"to many file to parse in: ",path) ;
 			if (!file_readputsa(&src,path,gaistr(&toparse,i))) strerr_diefu4sys(111,"read file: ",path,"/",gaistr(&toparse,i)) ;
 			if (!env_parsenclean(&modifs,&src)) strerr_diefu4x(111,"parse and clean environment of: ",path,"/",gaistr(&toparse,i)) ;
 			if (!env_split(&GAENV,&SAENV,&src)) strerr_diefu4x(111,"split environment of: ",path,"/",gaistr(&toparse,i)) ;
+			if (genalloc_len(stralist,&toparse) > n) strerr_dief4x(111,"to many variables in file: ",path,"/",gaistr(&toparse,i)) ;
 		}
 	}
 	genalloc_deepfree(stralist,&toparse,stra_free) ;
@@ -151,7 +201,6 @@ int main (int argc, char const *const *argv, char const *const *envp)
 	
 	for (i = 0 ; i < genalloc_len(diuint32,&GAENV) ; i++)
 	{
-			if (i > MAXVAR) strerr_dief1x(111,"to many variables") ;
 			unexport = 0 ;
 			int key = genalloc_s(diuint32,&GAENV)[i].left ;
 			int val = genalloc_s(diuint32,&GAENV)[i].right ;
@@ -160,7 +209,9 @@ int main (int argc, char const *const *argv, char const *const *envp)
 				key++ ;
 				unexport = 1 ;
 			}
-			if (!env_substitute(SAENV.s + key,SAENV.s + val,&info,newenv,unexport)) strerr_diefu4x(111,"substitute value of: ",SAENV.s + key," by: ",SAENV.s + val) ;
+			if(!loop_stra(&info.vars,SAENV.s + key))
+				if (!env_substitute(SAENV.s + key,SAENV.s + val,&info,newenv,unexport)) 
+					strerr_diefu4x(111,"substitute value of: ",SAENV.s + key," by: ",SAENV.s + val) ;
 	}
 	genalloc_free(diuint32,&GAENV) ;
 	stralloc_free(&SAENV) ;
