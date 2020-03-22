91e470872c23a99cf6c4341aaac4a8141232e40a
obarun <eric@obarun.org>
2020-03-23 09:09:57 +1100

    add version field at ss_resolve_s, add ss_resolve_module_path function. Change SS_NOTYPE to -1

--- src/include/66/resolve.h
+++ src/include/66/resolve.h
@@ -16,11 +16,11 @@
 #define SS_RESOLVE_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 #include <skalibs/genalloc.h>
 #include <skalibs/stralloc.h>
 #include <skalibs/types.h>
-#include <skalibs/uint32.h>
 #include <66/ssexec.h>
 #include <66/parser.h>
 
@@ -30,7 +30,7 @@
 #define SS_RESOLVE_SRC 1
 #define SS_RESOLVE_BACK 2
 #define SS_RESOLVE_STATE 3
-#define SS_NOTYPE 0
+#define SS_NOTYPE -1
 #define SS_SIMPLE 0
 #define SS_DOUBLE 1
 
@@ -42,6 +42,7 @@ struct ss_resolve_s
 	
 	uint32_t name ;
 	uint32_t description ;
+	uint32_t version ;
 	uint32_t logger ;
 	uint32_t logreal ;
 	uint32_t logassoc ;
@@ -66,7 +67,7 @@ struct ss_resolve_s
 	uint32_t down ;
 	uint32_t disen ;//disable->0,enable->1
 } ;
-#define RESOLVE_ZERO { 0,STRALLOC_ZERO,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+#define RESOLVE_ZERO { 0,STRALLOC_ZERO,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
 
 /** Graph struct */
 typedef struct ss_resolve_graph_ndeps_s ss_resolve_graph_ndeps_t ;
@@ -98,8 +99,9 @@ extern ss_resolve_t const ss_resolve_zero ;
 extern void ss_resolve_init(ss_resolve_t *res) ;
 extern void ss_resolve_free(ss_resolve_t *res) ;
 
-extern int ss_resolve_pointo(stralloc *sa,ssexec_t *info,unsigned int type, unsigned int where) ;
-extern int ss_resolve_src_path(stralloc *sasrc,char const *sv, ssexec_t *info) ;
+extern int ss_resolve_pointo(stralloc *sa,ssexec_t *info,int type, unsigned int where) ;
+extern int ss_resolve_src_path(stralloc *sasrc,char const *sv, uid_t owner) ;
+extern int ss_resolve_module_path(stralloc *sdir, stralloc *mdir, char const *sv, uid_t owner) ;
 extern int ss_resolve_src(stralloc *sasrc, char const *name, char const *src,int *found) ;
 extern int ss_resolve_service_isdir(char const *dir, char const *name) ;
 extern int ss_resolve_add_uint32(stralloc *sa, uint32_t data) ;
