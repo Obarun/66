6cae4a2e960d21717c9256fbb745a9593cf0af76
obarun <eric@obarun.org>
2020-06-09 17:57:15 +1100

    most useful commit framboise

--- src/lib66/ssexec_main.c
+++ src/lib66/ssexec_main.c
@@ -24,23 +24,21 @@
 #include <66/utils.h>
 #include <66/tree.h>
 
-#include <stdio.h>
-
 void set_ssinfo(ssexec_t *info)
 {
 	int r ;
-	
+
 	info->owner = MYUID ;
-	
+
 	if (!set_ownersysdir(&info->base,info->owner)) log_dieusys(LOG_EXIT_SYS, "set owner directory") ;
-	
+
 	r = tree_sethome(&info->tree,info->base.s,info->owner) ;
 	if (r < 0) log_dieu(LOG_EXIT_USER,"find the current tree. You must use -t options") ;
 	if (!r) log_dieusys(LOG_EXIT_SYS,"find tree: ", info->tree.s) ;
-	
+
 	r = tree_setname(&info->treename,info->tree.s) ;
 	if (r < 0) log_dieu(LOG_EXIT_SYS,"set the tree name") ;
-	
+
 	if (!tree_get_permissions(info->tree.s,info->owner))
 		log_die(LOG_EXIT_USER,"You're not allowed to use the tree: ",info->tree.s) ;
 	else info->treeallow = 1 ;
@@ -48,14 +46,14 @@ void set_ssinfo(ssexec_t *info)
 	r = set_livedir(&info->live) ;
 	if (!r) log_die_nomem("stralloc") ;
 	if(r < 0) log_die(LOG_EXIT_SYS,"live: ",info->live.s," must be an absolute path") ;
-	
+
 	if (!stralloc_copy(&info->livetree,&info->live)) log_die_nomem("stralloc") ;
 	if (!stralloc_copy(&info->scandir,&info->live)) log_die_nomem("stralloc") ;
-	
+
 	r = set_livetree(&info->livetree,info->owner) ;
 	if (!r) log_die_nomem("stralloc") ;
 	if(r < 0) log_die(LOG_EXIT_SYS,"livetree: ",info->livetree.s," must be an absolute path") ;
-	
+
 	r = set_livescan(&info->scandir,info->owner) ;
 	if (!r) log_die_nomem("stralloc") ;
 	if(r < 0) log_die(LOG_EXIT_SYS,"scandir: ",info->scandir.s," must be an absolute path") ;
@@ -67,16 +65,14 @@ static inline void info_help (char const *help)
 		log_dieusys(LOG_EXIT_SYS, "write to stdout") ;
 }
 
-
-
 int ssexec_main(int argc, char const *const *argv,char const *const *envp,ssexec_func_t *func, ssexec_t *info)
 {
 	int r ;
 	int n = 0 ;
 	char const *nargv[argc + 1] ;
-	
+
 	log_color = &log_color_disable ;
-	
+
 	nargv[n++] = "fake_name" ;
 	{
 		subgetopt_t l = SUBGETOPT_ZERO ;
@@ -84,7 +80,7 @@ int ssexec_main(int argc, char const *const *argv,char const *const *envp,ssexec
 		for (;;)
 		{
 			int opt = subgetopt_r(argc,argv, "hv:l:t:T:z", &l) ;
-			
+
 			if (opt == -1) break ;
 			switch (opt)
 			{
@@ -107,11 +103,11 @@ int ssexec_main(int argc, char const *const *argv,char const *const *envp,ssexec
 							info->opt_color = 1 ;
 							break ;
 				default	:	for (int i = 0 ; i < n ; i++)
-							{						
+							{
 								if (!argv[l.ind]) log_usage(info->usage) ;
 								if (obstr_equal(nargv[i],argv[l.ind]))
 									f = 1 ;
-							} 
+							}
 							if (!f) nargv[n++] = argv[l.ind] ;  
 							f = 0 ;
 							break ;
@@ -122,15 +118,13 @@ int ssexec_main(int argc, char const *const *argv,char const *const *envp,ssexec
 	set_ssinfo(info) ;
 
 	for (int i = 0 ; i < argc ; i++ , argv++)
-			nargv[n++] = *argv ;
-	
+		nargv[n++] = *argv ;
+
 	nargv[n] = 0 ;
-	
+
 	r = (*func)(n,nargv,envp,info) ;
-	
+
 	ssexec_free(info) ;
-		
+
 	return r ;
 }
-	
-
