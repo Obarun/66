9c9a09aae8c272b96fa64c147e4625cbebe79463
obarun <eric@obarun.org>
2022-01-15 19:54:13 +1100

    update deps file. Add gen-deps-libs.sh tool

---
+++ tools/gen-deps-libs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+for dir in src/lib66/* ; do
+
+    rm $dir/deps-lib/deps
+    for file in $(ls -1 $dir | grep -- \\.c$); do
+        echo $file | sed s/\\.c$/.o/ >> $dir/deps-lib/deps
+    done
+    echo "-ls6rc
+-ls6
+-loblibs
+-lexecline
+-lskarnet
+" >> $dir/deps-lib/deps
+done
