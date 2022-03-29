21f6d87840c5b910954f9195f8075b93b5c0e224
obarun <eric@obarun.org>
2022-03-29 19:08:26 +1100

    rename tree_resolve_master_field_tosa to tree_resolve_master_get_field_tosa

--- src/lib66/tree/tree_resolve_master_get_field_tosa.c
+++ src/lib66/tree/tree_resolve_master_get_field_tosa.c
@@ -1,5 +1,5 @@
 /*
- * tree_resolve_master_field_tosa.c
+ * tree_resolve_master_get_field_tosa.c
  *
  * Copyright (c) 2018-2021 Eric Vidal <eric@obarun.org>
  *
@@ -19,8 +19,8 @@
 #include <skalibs/types.h>//UINT_FMT
 
 #include <66/tree.h>
-
-int tree_resolve_master_field_tosa(stralloc *sa, resolve_tree_master_t *mres, resolve_tree_master_enum_t field)
+#include <stdio.h>
+int tree_resolve_master_get_field_tosa(stralloc *sa, resolve_tree_master_t *mres, resolve_tree_master_enum_t field)
 {
     log_flow() ;
 
@@ -30,23 +30,32 @@ int tree_resolve_master_field_tosa(stralloc *sa, resolve_tree_master_t *mres, re
     switch(field) {
 
         case TREE_ENUM_MASTER_NAME:
-            mres->sa.s + mres->name ;
+            str = mres->sa.s + mres->name ;
             break ;
 
         case TREE_ENUM_MASTER_ALLOW:
-            mres->sa.s + mres->name ;
+            str = mres->sa.s + mres->allow ;
             break ;
 
         case TREE_ENUM_MASTER_ENABLED:
-            mres->sa.s + mres->enabled ;
+            str = mres->sa.s + mres->enabled ;
             break ;
 
         case TREE_ENUM_MASTER_CURRENT:
-            mres->sa.s + mres->current ;
+            str = mres->sa.s + mres->current ;
+            break ;
+
+        case TREE_ENUM_MASTER_CONTENTS:
+            str = mres->sa.s + mres->contents ;
             break ;
 
         case TREE_ENUM_MASTER_NENABLED:
             fmt[uint32_fmt(fmt,mres->nenabled)] = 0 ;
+            str = fmt ;
+            break ;
+
+        case TREE_ENUM_MASTER_NCONTENTS:
+            fmt[uint32_fmt(fmt,mres->ncontents)] = 0 ;
             str = fmt ;
             break ;
 
