7b6bacf69b52bbdf037ae1758d1da7c55fc65b24
obarun <eric@obarun.org>
2022-03-29 19:16:35 +1100

    transfert resolve_service_field_table from service.c to service_resolve_modify_field.c file

--- src/lib66/service/service_resolve_modify_field.c
+++ src/lib66/service/service_resolve_modify_field.c
@@ -20,6 +20,44 @@
 #include <66/resolve.h>
 #include <66/service.h>
 
+resolve_field_table_t resolve_service_field_table[] = {
+
+    [SERVICE_ENUM_NAME] = { .field = "name" },
+    [SERVICE_ENUM_DESCRIPTION] = { .field = "description" },
+    [SERVICE_ENUM_VERSION] = { .field = "version" },
+    [SERVICE_ENUM_LOGGER] = { .field = "logger" },
+    [SERVICE_ENUM_LOGREAL] = { .field = "logreal" },
+    [SERVICE_ENUM_LOGASSOC] = { .field = "logassoc" },
+    [SERVICE_ENUM_DSTLOG] = { .field = "dstlog" },
+    [SERVICE_ENUM_DEPENDS] = { .field = "depends" },
+    [SERVICE_ENUM_REQUIREDBY] = { .field = "requiredby" },
+    [SERVICE_ENUM_OPTSDEPS] = { .field = "optsdeps" },
+    [SERVICE_ENUM_EXTDEPS] = { .field = "extdeps" },
+    [SERVICE_ENUM_CONTENTS] = { .field = "contents" },
+    [SERVICE_ENUM_SRC] = { .field = "src" },
+    [SERVICE_ENUM_SRCONF] = { .field = "srconf" },
+    [SERVICE_ENUM_LIVE] = { .field = "live" },
+    [SERVICE_ENUM_RUNAT] = { .field = "runat" },
+    [SERVICE_ENUM_TREE] = { .field = "tree" },
+    [SERVICE_ENUM_TREENAME] = { .field = "treename" },
+    [SERVICE_ENUM_STATE] = { .field = "state" },
+    [SERVICE_ENUM_EXEC_RUN] = { .field = "exec_run" },
+    [SERVICE_ENUM_EXEC_LOG_RUN] = { .field = "exec_log_run" },
+    [SERVICE_ENUM_REAL_EXEC_RUN] = { .field = "real_exec_run" },
+    [SERVICE_ENUM_REAL_EXEC_LOG_RUN] = { .field = "real_exec_log_run" },
+    [SERVICE_ENUM_EXEC_FINISH] = { .field = "exec_finish" },
+    [SERVICE_ENUM_REAL_EXEC_FINISH] = { .field = "real_exec_finish" },
+    [SERVICE_ENUM_TYPE] = { .field = "type" },
+    [SERVICE_ENUM_NDEPENDS] = { .field = "ndepends" },
+    [SERVICE_ENUM_NREQUIREDBY] = { .field = "nrequiredby" },
+    [SERVICE_ENUM_NOPTSDEPS] = { .field = "noptsdeps" },
+    [SERVICE_ENUM_NEXTDEPS] = { .field = "nextdeps" },
+    [SERVICE_ENUM_NCONTENTS] = { .field = "ncontents" },
+    [SERVICE_ENUM_DOWN] = { .field = "down" },
+    [SERVICE_ENUM_DISEN] = { .field = "disen" },
+    [SERVICE_ENUM_ENDOFKEY] = { .field = 0 }
+} ;
+
 int service_resolve_modify_field(resolve_service_t *res, resolve_service_enum_t field, char const *data)
 {
     log_flow() ;
