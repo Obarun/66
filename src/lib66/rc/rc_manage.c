66b49dae0e39c16b31d2599edeb1b778ffa9e569
obarun <eric@obarun.org>
2022-06-20 11:36:18 +1100

    avoid HEAP allocation

--- src/lib66/rc/rc_manage.c
+++ src/lib66/rc/rc_manage.c
@@ -34,88 +34,74 @@
 /**@Return 1 on success
  * @Return 2 on empty database
  * @Return 0 on fail */
-int rc_manage(ssexec_t *info,genalloc *ga)
+int rc_manage(ssexec_t *info, resolve_service_t *sv, unsigned int len)
 {
     log_flow() ;
 
     int r ;
+    unsigned int pos = 0 ;
     ss_state_t sta = STATE_ZERO ;
-    stralloc sares = STRALLOC_ZERO ;
     size_t newlen ;
 
-    tain deadline ;
-    if (info->timeout) tain_from_millisecs(&deadline, info->timeout) ;
-    else deadline = tain_infinite_relative ;
-    tain_now_g() ;
-    tain_add_g(&deadline, &deadline) ;
-
     char prefix[info->treename.len + 2] ;
-    memcpy(prefix,info->treename.s,info->treename.len) ;
-    memcpy(prefix + info->treename.len,"-",1) ;
-    prefix[info->treename.len + 1] = 0 ;
+    auto_strings(prefix, info->treename.s, "-") ;
 
     char live[info->livetree.len + 1 + info->treename.len + 1 ];
-    memcpy(live,info->livetree.s,info->livetree.len) ;
-    live[info->livetree.len] = '/' ;
-    memcpy(live + info->livetree.len + 1,info->treename.s,info->treename.len) ;
-    live[info->livetree.len + 1 + info->treename.len] = 0 ;
+    auto_strings(live, info->livetree.s, "/", info->treename.s) ;
 
-    if (!sa_pointo(&sares,info,TYPE_LONGRUN,SS_RESOLVE_SRC))
-    {
-        log_warnusys("set revolve pointer to source") ;
-        goto err ;
-    }
-    sares.len--;
-    if (!stralloc_cats(&sares,"/")) goto err ;
-    if (!stralloc_cats(&sares,info->treename.s)) goto err ;
-    if (!stralloc_cats(&sares,"/")) goto err ;
-    if (!stralloc_cats(&sares,SS_SVDIRS)) goto err ;
-    if (!stralloc_cats(&sares,"/")) goto err ;
-    newlen = sares.len ;
-    for (unsigned int i = 0 ; i < genalloc_len(resolve_service_t,ga) ; i++)
-    {
-        char const *string = genalloc_s(resolve_service_t,ga)[i].sa.s ;
-        char const *name = string + genalloc_s(resolve_service_t,ga)[i].name  ;
-        char const *runat = string + genalloc_s(resolve_service_t,ga)[i].runat ;
-        int type = genalloc_s(resolve_service_t,ga)[i].type ;
+    res_s[info->tree.len + SS_SVDIRS_LEN + SS_DB + 1 + info->treename.len + SS_SVDIRS_LEN + 1 + SS_MAX_SERVICE_NAME + 1] ;
+
+    auto_strings(res_s, info->tree.s, SS_SVDIRS, SS_DB, "/", info->treename.s, SS_SVDIRS, "/") ;
+
+    newlen = info->tree.len + SS_SVDIRS_LEN + SS_DB_LEN + 1 + info->treename.len + SS_SVDIRS_LEN + 1 ;
+
+    for (; pos < len ; pos++) {
+
+        char const *string = sv[pos].sa.s ;
+        char const *name = string + sv[pos].name  ;
+        char const *runat = string + sv[pos].runat ;
+        int type = sv[pos].type ;
+
         //do not try to copy a bundle or oneshot, this is already done.
-        if (type != TYPE_LONGRUN) continue ;
-        sares.len = newlen ;
-        if (!stralloc_cats(&sares,name)) goto err ;
-        if (!stralloc_0(&sares)) goto err ;
-        if (!hiercopy(sares.s,runat)) goto err ;
+        if (type != TYPE_LONGRUN)
+            continue ;
+
+        auto_strings(res_s + newlen, name) ;
+        if (!hiercopy(res_s,runat))
+            log_warnusys_return(LOG_EXIT_ZERO, "copy: ", res_s, " to: ", runat) ;
     }
+
     /** do not really init the service if you come from backup,
      * s6-rc-update will do the manage proccess for us. If we pass through
-     * here a double copy of the same service is made and s6-rc-update  will fail. */
-    r = db_find_compiled_state(info->livetree.s,info->treename.s) ;
-    if (!r)
-    {
+     * here a double copy of the same service is made and s6-rc-update will fail. */
+    r = db_find_compiled_state(info->livetree.s, info->treename.s) ;
+    if (!r) {
+
+        tain deadline ;
+        if (info->timeout)
+            tain_from_millisecs(&deadline, info->timeout) ;
+        else deadline = tain_infinite_relative ;
+
+        tain_now_g() ;
+        tain_add_g(&deadline, &deadline) ;
+
         r = s6rc_servicedir_manage_g(live, prefix, &deadline) ;
         if (r == -1)
-        {
-            log_warnusys("supervise service directories in ", live, "/servicedirs") ;
-            goto err ;
-        }
+            log_warnusys_return(LOG_EXIT_ZERO, "supervise service directories at: ", live, "/servicedirs") ;
     }
-    for (unsigned int i = 0 ; i < genalloc_len(resolve_service_t,ga) ; i++)
-    {
-        char const *string = genalloc_s(resolve_service_t,ga)[i].sa.s ;
-        char const *name = string + genalloc_s(resolve_service_t,ga)[i].name  ;
-        char const *state = string + genalloc_s(resolve_service_t,ga)[i].state ;
-        log_trace("Write state file of: ",name) ;
-        if (!state_write(&sta,state,name))
-        {
-            log_warnusys("write state file of: ",name) ;
-            goto err ;
-        }
+
+    for (pos = 0 ; i < len ; pos++) {
+
+        char const *string = sv[pos].sa.s ;
+        char const *name = string + sv[pos].name  ;
+        char const *state = string + sv[pos].state ;
+        log_trace("Write state file of: ", name) ;
+        if (!state_write(&sta, state, name))
+            log_warnusys_return(LOG_EXIT_ZERO, "write state file of: ", name) ;
+
         log_info("Initialized successfully: ",name) ;
     }
 
-    stralloc_free(&sares) ;
     return 1 ;
-    err:
-        stralloc_free(&sares) ;
-        return 0 ;
 }
 
