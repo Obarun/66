848ea177fe5f67bf33fd23ebd66f40c3d6458143
obarun <eric@obarun.org>
2023-05-21 20:50:34 +1100

    do not remove event supervise directory if the service is supervised

--- src/lib66/sanitize/sanitize_write.c
+++ src/lib66/sanitize/sanitize_write.c
@@ -14,11 +14,18 @@
 
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include <oblibs/log.h>
 #include <oblibs/directory.h>
 #include <oblibs/string.h>
 #include <oblibs/types.h>
+#include <oblibs/sastr.h>
+
+#include <skalibs/djbunix.h>
+#include <skalibs/unix-transactional.h>
 
 #include <66/sanitize.h>
 #include <66/resolve.h>
@@ -26,53 +33,49 @@
 #include <66/enum.h>
 #include <66/tree.h>
 #include <66/state.h>
+#include <66/svc.h>
 
-static int delete(resolve_service_t *res, uint8_t force)
+static void remove_dir(char const *path, char const *name)
 {
-    int r ;
+    size_t pos = 0 ;
+    stralloc sa = STRALLOC_ZERO ;
 
-    char dir[strlen(res->sa.s + res->path.servicedir) + 1] ;
+    char const *exclude[5] = { "event", "supervise", "scandir", 0 } ;
+    if (!sastr_dir_get_recursive(&sa, path, exclude, S_IFDIR|S_IFREG, 1))
+        log_dieusys(LOG_EXIT_SYS, "get list of running services") ;
 
-    auto_strings(dir, res->sa.s + res->path.servicedir) ;
+    FOREACH_SASTR(&sa, pos) {
+        log_trace("remove element: ", sa.s + pos, " from: ", name, " service directory") ;
 
-    r = scan_mode(dir, S_IFDIR) ;
-    if (r < 0)
-        log_die(LOG_EXIT_SYS, "unvalid source: ", dir) ;
-
-    if (r && force) {
-
-        log_trace("remove directory: ", dir) ;
-
-        if (!dir_rm_rf(dir))
-            log_dieusys(LOG_EXIT_SYS, "remove: ", dir) ;
-
-    } else if (r && !force) {
-
-        log_info("Ignoring: ", res->sa.s + res->name, " service: already written") ;
-        return 2 ;
+        if (!dir_rm_rf(sa.s + pos))
+            log_dieusys(LOG_EXIT_SYS, "remove: ", sa.s + pos) ;
     }
 
-    return 1 ;
+    stralloc_free(&sa) ;
 }
 
 static void resolve_compare(resolve_service_t *res)
 {
+    log_flow() ;
+
+    int r ;
     resolve_service_t fres = RESOLVE_SERVICE_ZERO ;
     resolve_wrapper_t_ref wres = resolve_set_struct(DATA_SERVICE, &fres) ;
-    ss_state_t ste = STATE_ZERO ;
+    ss_state_t sta = STATE_ZERO ;
     char *name = res->sa.s + res->name ;
 
-    if (resolve_check_g(wres, res->sa.s + res->path.home, name)) {
+    r = resolve_read_g(wres, res->sa.s + res->path.home, name) ;
+    if (r < 0)
+        log_dieu(LOG_EXIT_SYS, "read resolve file of: ", name) ;
 
-        if (!resolve_read_g(wres, res->sa.s + res->path.home, name))
-            log_dieu(LOG_EXIT_SYS, "read resolve file of: ", name) ;
+    if (r) {
 
-        if (state_check(&fres)) {
+        if (!state_read(&sta, &fres))
+            log_dieu(LOG_EXIT_SYS, "read state file of: ", name) ;
 
-            if (!state_read(&ste, &fres))
-                log_dieu(LOG_EXIT_SYS, "read state file of: ", name) ;
+        if (service_is(&sta, STATE_FLAGS_ISSUPERVISED) == STATE_FLAGS_TRUE) {
 
-            if (fres.type != res->type && FLAGS_ISSET(ste.issupervised, STATE_FLAGS_TRUE))
+            if (fres.type != res->type)
                 log_die(LOG_EXIT_SYS, "Detection of incompatible type format for supervised service: ", name, " -- current: ", get_key_by_enum(ENUM_TYPE, res->type), " previous: ", get_key_by_enum(ENUM_TYPE, fres.type), ". Please unsupervise it with '66 unsupervice ", name,"' before trying the conversion") ;
         }
 
@@ -83,39 +86,108 @@ static void resolve_compare(resolve_service_t *res)
     resolve_free(wres) ;
 }
 
+static int delete(resolve_service_t *res, uint8_t force)
+{
+    log_flow() ;
+
+    int r ;
+
+    char dir[strlen(res->sa.s + res->path.servicedir) + 1] ;
+
+    auto_strings(dir, res->sa.s + res->path.servicedir) ;
+
+    r = scan_mode(dir, S_IFDIR) ;
+    if (r < 0)
+        log_die(LOG_EXIT_SYS, "unvalid source: ", dir) ;
+
+    if (r) {
+
+        if (force) {
+
+            stralloc sa = STRALLOC_ZERO ;
+            ss_state_t sta = STATE_ZERO ;
+
+            /** Keep the current symlink target. */
+            if (sareadlink(&sa, dir) < 0 ||
+                !stralloc_0(&sa))
+                    log_dieusys(LOG_EXIT_SYS, "readlink: ", dir) ;
+
+            resolve_compare(res) ;
+
+            if (state_check(res)) {
+
+                if (!state_read(&sta, res))
+                    log_dieu(LOG_EXIT_SYS, "read state file of: ", res->sa.s + res->name) ;
+            }
+
+            remove_dir(sa.s, res->sa.s + res->name) ;
+
+            if (!state_write(&sta, res))
+                log_dieu(LOG_EXIT_SYS, "write state file of: ", res->sa.s + res->name) ;
+
+            stralloc_free(&sa) ;
+
+        } else
+            /** This info should never be executed as long as the parse_frontend
+             * check already verifies the service and prevents reaching this point if !force. */
+            log_info_return(2, "Ignoring: ", res->sa.s + res->name, " -- service already written") ;
+    }
+
+    return 1 ;
+}
+
+static void make_servicedir(resolve_service_t *res)
+{
+    log_flow() ;
+
+    int r ;
+    char *name = res->sa.s + res->name ;
+    size_t namelen = strlen(name), homelen = strlen(res->sa.s + res->path.home) ;
+    char sym[homelen + SS_SYSTEM_LEN + SS_RESOLVE_LEN + SS_SERVICE_LEN + 1 + namelen + 1] ;
+    char dst[homelen + SS_SYSTEM_LEN + SS_SERVICE_LEN + SS_SVC_LEN + SS_RESOLVE_LEN + 1 + namelen + 1] ;
+
+    auto_strings(sym, res->sa.s + res->path.home, SS_SYSTEM, SS_RESOLVE, SS_SERVICE, "/", name) ;
+
+    auto_strings(dst, res->sa.s + res->path.home, SS_SYSTEM, SS_SERVICE, SS_SVC, "/", name) ;
+
+    struct stat st ;
+    r = lstat(sym, &st) ;
+    if (r < 0) {
+
+        if (errno != ENOENT) {
+
+            log_dieusys(LOG_EXIT_SYS, "lstat: ", sym) ;
+
+        } else {
+
+            log_trace("symlink: ", sym, " to: ", dst) ;
+            r = symlink(dst, sym) ;
+            if (r < 0 && errno != EEXIST)
+                log_dieusys(LOG_EXIT_SYS, "point symlink: ", sym, " to: ", dst) ;
+        }
+    }
+
+    auto_strings(dst + homelen + SS_SYSTEM_LEN + SS_SERVICE_LEN + SS_SVC_LEN + 1 + namelen, SS_RESOLVE) ;
+
+    r = scan_mode(dst, S_IFDIR) ;
+    if (r < 0)
+        log_die(LOG_EXIT_SYS, "unvalid source: ", dst) ;
+
+    log_trace("create directory: ", dst) ;
+    if (!dir_create_parent(dst, 0755))
+        log_dieusys(LOG_EXIT_SYS, "create directory: ", dst) ;
+}
+
 void sanitize_write(resolve_service_t *res, uint8_t force)
 {
-    /** create symlink to .resolve/ directory */
-    int r ;
-    size_t homelen = strlen(res->sa.s + res->path.servicedir) ;
-
-    resolve_compare(res) ;
+    log_flow() ;
 
     if (delete(res, force) > 1)
         return ;
 
-    char sym[homelen + SS_RESOLVE_LEN + 1] ;
+    make_servicedir(res) ;
 
-    auto_strings(sym, res->sa.s + res->path.servicedir) ;
-
-    r = scan_mode(sym, S_IFDIR) ;
-    if (r < 0)
-        log_die(LOG_EXIT_SYS, "unvalid source: ", sym) ;
-
-    log_trace("create directory: ", sym) ;
-    if (!dir_create_parent(sym, 0755))
-        log_dieusys(LOG_EXIT_SYS, "create directory: ", sym) ;
-
-    char dst[SS_RESOLVE_LEN + 5 + 1] ;
-
-    auto_strings(dst, "../.." SS_RESOLVE) ;
-    auto_strings(sym + homelen, SS_RESOLVE) ;
-
-    log_trace("symlink: ", sym, " to: ", dst) ;
-    r = symlink(dst, sym) ;
-    if (r < 0 && errno != EEXIST)
-        log_dieusys(LOG_EXIT_SYS, "point symlink: ", sym, " to: ", dst) ;
-
-
+    if (!service_resolve_write(res))
+        log_dieu(LOG_EXIT_SYS, "write resolve file of service: ", res->sa.s + res->name) ;
 }
 
