a1d44cc076f6a82b7d71b12342b0771a8ec8fb14
obarun <eric@obarun.org>
2023-10-31 18:34:52 +1100

    fix check of earlier service

--- src/lib66/sanitize/sanitize_scandir.c
+++ src/lib66/sanitize/sanitize_scandir.c
@@ -99,7 +99,7 @@ int sanitize_scandir(resolve_service_t *res, ss_state_t *sta)
         if (res->type == TYPE_CLASSIC)
             scandir_scandir_to_livestate(res) ;
 
-        if (res->earlier) {
+        if (!res->earlier) {
 
             if (svc_scandir_send(svcandir, "h") <= 0)
                 log_warnu_return(LOG_EXIT_ZERO, "reload scandir: ", svcandir) ;
