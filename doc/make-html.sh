ba82ead5d2e160e4b97c8c448f2f766794080911
obarun <eric@obarun.org>
2023-12-11 13:09:45 +1100

    provide version doc

--- doc/make-html.sh
+++ doc/make-html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-html='66-echo 66-hpr 66-nuke 66-shutdown 66-shutdownd 66-umountall 66 boot configure deeper disable enable execl-envfile frontend halt index init instantiated-service module-creation module-usage parse poweroff reboot reconfigure reload remove resolve restart rosetta scandir service-configuration-file signal start state status stop tree upgrade'
+html='66-echo 66-hpr 66-nuke 66-shutdown 66-shutdownd 66-umountall 66 boot configure deeper disable enable execl-envfile frontend halt index init instantiated-service module-creation module-usage parse poweroff reboot reconfigure reload remove resolve restart rosetta scandir service-configuration-file signal start state status stop tree upgrade version'
 
 version=${1}
 
---
+++ doc/version.md
@@ -0,0 +1,30 @@
+title: The 66 Suite: version
+author: Eric Vidal <eric@obarun.org>
+
+[66](index.html)
+
+[Software](https://web.obarun.org/software)
+
+[obarun.org](https://web.obarun.org)
+
+# version
+
+This command displays the version of `66`.
+
+## Interface
+
+```
+version
+```
+
+## Options
+
+No options
+
+## Usage example
+
+Display the installed `66` version
+
+```
+66 version
+```
